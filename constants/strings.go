8c76eb18665b6c153c4928fe9ab6702b1976a6ec
slipneff@mail.ru <slipneff@mail.ru>
2024-05-22 14:41:33 +0300

    WIP

--- constants/strings.go
+++ constants/strings.go
@@ -1,3 +1,3 @@
 package constants
 
-const Hello = "Привет! Я - сервис по подбору респондентов в Telegram. Если вы хотите провести глубинное интервью или же принять участие в стороннем исследовании с возможностью получения его результата, то этот бот для вас\n\nhttps://docs.google.com/document/d/1xdPl9pG4eNcBSz4_I5QjWSFVQOIF5HwjJEanoLmNPmQ"
+const Hello = "Привет! Я - сервис по подбору респондентов в Telegram. Если вы хотите провести глубинное интервью или же принять участие в стороннем исследовании с возможностью получения его результата, то этот бот для вас\n\nИспользуя данный сервис вы принимаете оферту:\nhttps://docs.google.com/document/d/1xdPl9pG4eNcBSz4_I5QjWSFVQOIF5HwjJEanoLmNPmQ"
